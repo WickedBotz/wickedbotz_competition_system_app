552294152c705bd4340356d7d3d31b7d921b779b
jeziel nogueira <jezielnogueira_15@yahoo.com.br>
2024-11-03 19:09:25 -0300

    added search box

--- lib/pages/matchs_page.dart
+++ lib/pages/matchs_page.dart
@@ -11,7 +11,7 @@ import '../data/models/user_model.dart';
 import '../data/provider/user_provider.dart';
 import 'combat_page.dart';
 
-class MatchsPage extends StatefulWidget{
+class MatchsPage extends StatefulWidget {
   final CategoriesModel Category;
   final CompetitionsModel Competiotion;
   const MatchsPage({super.key, required this.Category, required this.Competiotion});
@@ -20,13 +20,14 @@ class MatchsPage extends StatefulWidget{
   State<MatchsPage> createState() => _MatchsPage();
 }
 
-class _MatchsPage extends State<MatchsPage>{
-
+class _MatchsPage extends State<MatchsPage> {
   late CategoryMatchStore store;
   late UserModel user;
+  TextEditingController searchController = TextEditingController();
+  String searchQuery = '';
 
   @override
-  void initState(){
+  void initState() {
     super.initState();
     print('category_id: ${widget.Category.category_id}');
   }
@@ -44,6 +45,12 @@ class _MatchsPage extends State<MatchsPage>{
     );
 
     store.getRobotsMatch(category_id: widget.Category.category_id);
+  }
+
+  void _onSearchChanged() {
+    setState(() {
+      searchQuery = searchController.text.toLowerCase();
+    });
   }
 
   @override
@@ -76,85 +83,116 @@ class _MatchsPage extends State<MatchsPage>{
           ),
         ],
       ),
-      body: AnimatedBuilder(
-          animation: Listenable.merge([
-            store.isLoading,
-            store.erro,
-            store.state,
-          ]),
-          builder: (context, child) {
-            if(store.isLoading.value){
-              return const CircularProgressIndicator();
-            }
-            if(store.erro.value.isNotEmpty){
-              print('Erro');
-              return Center(
-                child: Text(store.erro.value),
-              );
-            }
-            if(store.state.value.isEmpty){
-              return const Center(
-                child: Text('Nemhum dado encontrado'),
-              );
-            }else{
-              print('Loaded');
-              return ListView.separated(
-                separatorBuilder: (context, index) => const SizedBox(
-                  height: 32,
+      body: Column(
+        children: [
+          SizedBox(height: 20,),
+          Text('Categoria: ${widget.Category.category_name}',
+            style: TextStyle(fontSize: 25),),
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: TextField(
+              controller: searchController,
+              decoration: InputDecoration(
+                hintText: 'Nome do robô ou equipe',
+                hintStyle: TextStyle(fontSize: 16),
+                prefixIcon: Icon(Icons.search),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(10),
                 ),
-                padding: const EdgeInsets.all(16),
-                itemCount: store.state.value.length,
-                itemBuilder: (_, index){
-                  final item = store.state.value[index];
-                  if(item.sequence != 0){
-                    print('SOLO::::: ${item.match_id}' );
-                  }
-                  return GestureDetector(
-                    onTap: (){
-                      print('Match id: ${item.match_id}');
-                      print('Category id: ${widget.Category.category_id}');
-                      print('Match id: ${item.current}');
-                      print('Match id: ${item.sequence}');
-                      if(widget.Category.category_id == 2 || widget.Category.category_id == 3){
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                              builder: (context) => PursuitMatchPage(
-                                Match: item, Competiotion: widget.Competiotion, Category: widget.Category,
-                              )),
-                        );
-                      }
-                      else{
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                              builder: (context) => CombatMatchPage(
-                                Match: item, Competiotion: widget.Competiotion, Category: widget.Category,
-                              )),
-                        );
-                      }
-                    },
-                    child: Column(
-                      children: [
-                        ClipRRect(
-                          borderRadius: BorderRadius.circular(16),
-                          //child: Image.network(item.lf_photo),
-                          child: Image.network('https://images.ctfassets.net/cnu0m8re1exe/6fVCq8MwHs552WbNadncGb/1bd5a233597acb5485c691c8110270b2/shutterstock_710379334.jpg?fm=jpg&fl=progressive&w=660&h=433&fit=fill'),
-                        ),
-                        ListTile(
-                          contentPadding: EdgeInsets.zero,
-                          title: Text('Robots: ${item.robot_1_name} X ${item.robot_2_name}'),
-                          subtitle: Text('Teams: ${item.team_1_name} / ${item.team_2_name}'),
-                        ),
-
-                      ],
-                    ),
+              ),
+              onChanged: (text) => _onSearchChanged(),
+            ),
+          ),
+          Expanded(
+            child: AnimatedBuilder(
+              animation: Listenable.merge([
+                store.isLoading,
+                store.erro,
+                store.state,
+              ]),
+              builder: (context, child) {
+                if (store.isLoading.value) {
+                  return const Center(child: CircularProgressIndicator());
+                }
+                if (store.erro.value.isNotEmpty) {
+                  return Center(
+                    child: Text(store.erro.value),
                   );
-                },
-              );
-            }
-          }),
+                }
+
+                // Filtragem dos itens de acordo com o texto da busca
+                final filteredItems = store.state.value.where((item) {
+                  final robotNames = '${item.robot_1_name} ${item.robot_2_name}'.toLowerCase();
+                  final teamNames = '${item.team_1_name} ${item.team_2_name}'.toLowerCase();
+                  return robotNames.contains(searchQuery) || teamNames.contains(searchQuery);
+                }).toList();
+
+                if (filteredItems.isEmpty) {
+                  return const Center(
+                    child: Text('Nenhum dado encontrado'),
+                  );
+                } else {
+                  return ListView.separated(
+                    separatorBuilder: (context, index) => const SizedBox(
+                      height: 32,
+                    ),
+                    padding: const EdgeInsets.all(16),
+                    itemCount: filteredItems.length,
+                    itemBuilder: (_, index) {
+                      final item = filteredItems[index];
+                      if (item.sequence != 0) {
+                        print('SOLO::::: ${item.match_id}');
+                      }
+                      return GestureDetector(
+                        onTap: () {
+                          if (widget.Category.category_id == 2 || widget.Category.category_id == 3) {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) => PursuitMatchPage(
+                                  Match: item,
+                                  Competiotion: widget.Competiotion,
+                                  Category: widget.Category,
+                                ),
+                              ),
+                            );
+                          } else {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) => CombatMatchPage(
+                                  Match: item,
+                                  Competiotion: widget.Competiotion,
+                                  Category: widget.Category,
+                                ),
+                              ),
+                            );
+                          }
+                        },
+                        child: Column(
+                          children: [
+                            ClipRRect(
+                              borderRadius: BorderRadius.circular(16),
+                              child: Image.network(
+                                'https://images.ctfassets.net/cnu0m8re1exe/6fVCq8MwHs552WbNadncGb/1bd5a233597acb5485c691c8110270b2/shutterstock_710379334.jpg?fm=jpg&fl=progressive&w=660&h=433&fit=fill',
+                              ),
+                            ),
+                            ListTile(
+                              contentPadding: EdgeInsets.zero,
+                              title: Text('Robots: ${item.robot_1_name} X ${item.robot_2_name}'),
+                              subtitle: Text('Teams: ${item.team_1_name} / ${item.team_2_name}'),
+                            ),
+                          ],
+                        ),
+                      );
+                    },
+                  );
+                }
+              },
+            ),
+          ),
+        ],
+      ),
     );
   }
-
 }
